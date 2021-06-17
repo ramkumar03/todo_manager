ade237435d9fd8c839c48dd7da1f99a77d4a8eb9
ramkumar03 <roster99brock@gmail.com>
2021-06-17 11:55:55 +0530

    Add Ability to create new todo with response message

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -9,4 +9,13 @@ class TodosController < ApplicationController
     todo = Todo.find(id).to_pleasant_string
     render plain: todo
   end
+
+  def create
+    todo_text = params[:todo_text]
+    due_date = DateTime.parse(params[:due_date])
+    new_todo = Todo.create!(todo_text: todo_text, due_date: due_date, completed: false)
+
+    response_text = "Your new todo is created with id #{new_todo.id} \n \n #{Todo.find(new_todo.id).to_pleasant_string}"
+    render plain: response_text
+  end
 end
