1bbb4227e468f4ce29ce42dbb3e68564cd68b3ca
ramkumar03 <roster99brock@gmail.com>
2021-06-18 10:10:05 +0530

    add display and create user feature

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,18 @@
+# users_controller.rb
+class UsersController < ApplicationController
+  skip_before_action :verify_authenticity_token
+
+  def index
+    user = User.order(:id).map { |user| user.to_display_user }.join("\n")
+    render plain: "User List: - \n \n #{user}"
+  end
+
+  def create
+    name = params[:name]
+    email = params[:email]
+    password = params[:password]
+    new_user = User.create!(name: name, email: email, password: password)
+    response_text = "hey #{new_user.name}, \n Your User id created with id #{new_user.id}"
+    render plain: response_text
+  end
+end
---
+++ app/models/user.rb
@@ -0,0 +1,5 @@
+class User < ActiveRecord::Base
+  def to_display_user
+    "\t |\t #{id}.\t | \t #{name}  \t |\t #{email}\t |"
+  end
+end
