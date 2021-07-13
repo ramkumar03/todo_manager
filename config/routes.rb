094be289054e3696c8cf6939386cf2c6c34c6b06
ramkumar03 <roster99brock@gmail.com>
2021-07-13 19:11:46 +0530

    Created sign in page without functionality and homepage

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
-  root to: "home#index"
+  get "/" => "home#index"
+
+  get "/signin" => "sessions#new", as: :new_sessions
+  post "/signin" => "sessions#create", as: :sessions
   resources :todos
+
   resources :users
   post "users/login", to: "users#login"
 end
